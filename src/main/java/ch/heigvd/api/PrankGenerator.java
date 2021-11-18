c059583ab3149533d22bd7d816018df50fbee896
Luhrel <lucien.perregaux@gmail.com>
2021-11-18 10:49:07 +0100

    Initialize connection to smtp server

--- src/main/java/ch/heigvd/api/PrankGenerator.java
+++ src/main/java/ch/heigvd/api/PrankGenerator.java
@@ -1,7 +1,38 @@
 package ch.heigvd.api;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class PrankGenerator {
-    public static void main(String[] args) {
-        System.out.println("This is a prank !");
+  public static void main(String[] args) {
+    if (args.length < 2) {
+      System.err.println("Usage: main {host} {port}");
+      return;
     }
+
+    String host = args[0];
+    int port;
+    try {
+      port = Integer.parseInt(args[1]);
+    } catch (NumberFormatException e) {
+      System.err.println(e.getMessage());
+      return;
+    }
+
+    try {
+      Socket client = new Socket(host, port);
+
+      sendMail(client);
+
+      client.close();
+
+    } catch (IOException | IllegalArgumentException e) {
+      System.err.println(e.getMessage());
+    }
+  }
+
+  private static void sendMail(Socket client)
+  {
+    if (client == null) throw new IllegalArgumentException("Client socker is null !");
+  }
 }
