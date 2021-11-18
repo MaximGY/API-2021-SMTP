28864607a8c180c00b62cf22658521f5730f1565
Maxim Golay <maxim.golay@ik.me>
2021-11-18 11:26:57 +0100

    Implemented Message class

--- src/main/java/ch/heigvd/api/utils/Mail.java
+++ src/main/java/ch/heigvd/api/utils/Mail.java
@@ -6,13 +6,13 @@ public class Mail {
     public static String CRLF = "\r\n";
 
     private final Group group;
-    private final String body;
+    private final Message message;
 
 
-    Mail(Group group, String body) {
+    Mail(Group group, Message message) {
 
         this.group = group;
-        this.body = body;
+        this.message = message;
     }
 
     public String getSMTPCommand() {
@@ -27,7 +27,7 @@ public class Mail {
             sb.append("RCPT TO: <").append(rec.getMail()).append('>').append(CRLF);
 
         // TODO: add headers to body
-        sb.append("DATA").append(CRLF).append(body);
+        sb.append("DATA").append(CRLF).append(message.getBody());
 
         // End of mail
         sb.append(CRLF).append('.').append(CRLF);
---
+++ src/main/java/ch/heigvd/api/utils/Message.java
@@ -0,0 +1,20 @@
+package ch.heigvd.api.utils;
+
+public class Message {
+
+    private final String subject;
+    private final String body;
+
+    Message(String subject, String body) {
+        this.subject = subject;
+        this.body = body;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
