17c04b223d8aa11796d63b7b079a7c763b031f8b
Luhrel <lucien.perregaux@gmail.com>
2021-11-25 11:42:58 +0100

    Add getters in mail

--- src/main/java/ch/heigvd/api/utils/Mail.java
+++ src/main/java/ch/heigvd/api/utils/Mail.java
@@ -1,37 +1,19 @@
 package ch.heigvd.api.utils;
 
 public class Mail {
+  private final Group group;
+  private final Message message;
 
-    public static final String LF = "\n";
-    public static final String CRLF = "\r\n";
+  public Mail(Group group, Message message) {
+    this.group = group;
+    this.message = message;
+  }
 
-    private final Group group;
-    private final Message message;
+  public Group getGroup() {
+    return group;
+  }
 
-
-    public Mail(Group group, Message message) {
-
-        this.group = group;
-        this.message = message;
-    }
-
-    public String getSMTPCommand() {
-
-        StringBuilder sb = new StringBuilder();
-
-        // Mail sender
-        sb.append("MAIL FROM: <").append(group.getSender().getMail()).append('>').append(CRLF);
-
-        // Mail recipients
-        for (User rec: group.getRecipients())
-            sb.append("RCPT TO: <").append(rec.getMail()).append('>').append(CRLF);
-
-        // TODO: add headers to body
-        sb.append("DATA").append(CRLF).append(message.getBody());
-
-        // End of mail
-        sb.append(CRLF).append('.').append(CRLF);
-
-        return sb.toString();
-    }
+  public Message getMessage() {
+    return message;
+  }
 }
