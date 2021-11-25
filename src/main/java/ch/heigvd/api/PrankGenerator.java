2b0d3abdc4111df5e3b3fb9fb9a2bc327ace5900
Luhrel <lucien.perregaux@gmail.com>
2021-11-25 11:21:48 +0100

    Add generateGroups function

--- src/main/java/ch/heigvd/api/PrankGenerator.java
+++ src/main/java/ch/heigvd/api/PrankGenerator.java
@@ -8,10 +8,11 @@ import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class PrankGenerator {
+  static final Random random = new Random();
 
   public static void main(String[] args) {
     if (args.length < 4) {
-      System.err.println("Usage: ./main {host} {port} {victims.txt} {mail_body.txt}");
+      System.err.println("Usage: ./main {host} {port} {victims.txt} {mail_body.txt} {settings.txt}");
       return;
     }
 
@@ -20,11 +21,14 @@ public class PrankGenerator {
       String host = args[0];
       int port = Integer.parseInt(args[1]);
       ArrayList<User> victims = FileParser.getUsersFromFile(args[2]);
-      ArrayList<Message> message = FileParser.getMessagesFromFile(args[3]);
+      ArrayList<Message> messages = FileParser.getMessagesFromFile(args[3]);
+      int nbGroups = 3;
+      ArrayList<Group> groups = generateGroups(victims, nbGroups);
 
       Socket client = new Socket(host, port);
 
-      sendMail(client);
+
+      //sendMail(client);
 
       client.close();
 
@@ -33,7 +37,7 @@ public class PrankGenerator {
     }
   }
 
-  private static void sendMail(Socket client) throws IOException {
+  private static void sendMail(Socket client, Mail mail) throws IOException {
     if (client == null) throw new IllegalArgumentException("Client socket is null !");
 
     String msg = "";
@@ -43,5 +47,27 @@ public class PrankGenerator {
     var out =
         new BufferedWriter(
             new OutputStreamWriter(client.getOutputStream(), StandardCharsets.UTF_8));
+  }
+
+  private static ArrayList<Group> generateGroups(ArrayList<User> victims, int nbGroups) {
+    if (nbGroups <= 0) throw new RuntimeException("The number of groups must be greater than zéro.");
+
+    ArrayList<Group> groups = new ArrayList<>(nbGroups);
+
+    int nbVictimsPerGroup = victims.size() / nbGroups;
+    if (nbVictimsPerGroup < 3) throw new RuntimeException("Not enough victims !");
+
+    final int shift = random.nextInt();
+
+    for (int i = 0; i < victims.size(); ++i)
+    {
+      Group group = groups.get(i % nbGroups);
+      User user = victims.get((i + shift) % victims.size());
+      if (group.getSender() == null)
+        group.setSender(user);
+      else
+        group.addRecipient(user);
+    }
+    return groups;
   }
 }
